fe7d4a9acbf51ef6871580095cd7abdb81d6dea7
ReinaldoSD <reinaldosergiodsilva@gmail.com>
2026-04-21 11:59:40 -0300

    Update app.py

--- Vest.IA/app.py
+++ Vest.IA/app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from banco_dados.create_db import criar_banco
 from routes import configure_routes
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'imagens_roupas')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 criar_banco()
 configure_routes(app)
